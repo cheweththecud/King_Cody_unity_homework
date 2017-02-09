42fefb37ea2d62a6f23ba207a215f59c94dfcc86
cheweththecud <theelderking@yahoo.com>
2017-02-09 12:14:35 -0800

    added projectile

---
+++ Assets/ballshoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballshoot : MonoBehaviour {
+
+    public GameObject projectile;
+    public float speed = 20;
+    private GameObject clone;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * speed);
+        }
+    }
+}
