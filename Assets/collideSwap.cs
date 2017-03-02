9b770929af330b2f76690bafcb9b39503785dcba
cheweththecud <theelderking@yahoo.com>
2017-03-01 21:08:59 -0800

    added some scripts to objects
    having problems with unity so i changed some things to help it work
    better.

---
+++ Assets/collideSwap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collideSwap : MonoBehaviour {
+
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        Color color = Random.ColorHSV();
+        rend.material.SetColor("_Color", color);
+    }
+}
---
+++ Assets/ouch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ouch : MonoBehaviour
+{
+    public string reply = "ouch!";
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+       
+        if (collision.collider.name != "Terrain")
+        {
+            print(reply);
+        }
+    }
+}
