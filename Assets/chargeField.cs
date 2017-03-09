23a2fe4e4141cd46b3d99a2e7689198ce9c433a3
cheweththecud <theelderking@yahoo.com>
2017-03-09 12:41:03 -0800

    added loop script

---
+++ Assets/chargeField.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chargeField : MonoBehaviour {
+
+    //public GameObject post;
+    int height= 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (height == 20) {
+            Destroy(gameObject, 5);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name != "Terrain")
+        {
+            height += 1;
+            for (int i = 1; i < height; i++)
+            {
+                print(i);
+            }
+        }
+    }
+
+    
+
+
+
+
+
+    }
